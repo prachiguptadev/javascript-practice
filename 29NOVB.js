c48c7c5e85fa4afc23ee844c6f7182073caab18a
Prachi Gupta <117148255+prachiguptadev@users.noreply.github.com>
2023-12-02 17:32:34 +0000

    create a function cheak the minimum number

---
+++ 29NOVB.js
@@ -0,0 +1,16 @@
+
+// create a function cheak the minimum number
+
+function minNumber(num){
+    let minNum = num[0]
+    
+    for(let i of num){
+      if(minNum < num[i])               //cheak condition
+        minNum = num[i]                 //set the value of minNum
+        
+    }
+    
+  return minNum;
+  }
+  let numbers = [100, 15, 54, 20,24]     //numbers array
+  console.log(minNumber(numbers))
